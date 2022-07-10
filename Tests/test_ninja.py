2bad11d7488168d6eaffec54596b3596233c7858
Nagaraj <btnagaraj26@gmail.com>
2022-07-10 12:45:11 +0530

    second commit

--- Tests/test_ninja.py
+++ Tests/test_ninja.py
@@ -13,6 +13,11 @@ class Testninjaproject():
         driver.maximize_window()
         # fistr click
         driver.find_element(By.LINK_TEXT, "Phones & PDAs").click()
+
+            NAGAMALLIA,K,
+
+
+
         driver.find_element(By.XPATH, "//img[@title='iPhone']").click()
         driver.find_element(By.XPATH, "//ul/li[1]/a[1]/img[1]").click()
         right_button = driver.find_element(By.XPATH, "//button[@title='Next (Right arrow key)']")
